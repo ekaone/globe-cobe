1d8fbd5e247a4f1f2a322d078e2238382954756f
ekaone <ekaone3033@gmail.com>
2022-12-16 10:55:34 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useRef } from "react";
+import createGlobe from "cobe";
+
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    let phi = 0;
+
+    const globe = createGlobe(canvasRef.current, {
+      devicePixelRatio: 2,
+      width: 600 * 2,
+      height: 600 * 2,
+      phi: 0,
+      theta: 0,
+      dark: 1,
+      diffuse: 1.2,
+      mapSamples: 16000,
+      mapBrightness: 6,
+      baseColor: [0.3, 0.3, 0.3],
+      markerColor: [0.1, 0.8, 1],
+      glowColor: [1, 1, 1],
+      markers: [
+        // longitude latitude
+        { location: [-6.2, 106.816666], size: 0.1 },
+        { location: [40.7128, -74.006], size: 0.1 },
+      ],
+      onRender: (state) => {
+        // Called on every animation frame.
+        // `state` will be an empty object, return updated params.
+        state.phi = phi;
+        phi += 0.01;
+      },
+    });
+
+    return () => {
+      globe.destroy();
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>COBE</h1>
+      <p>
+        A lightweight (5kB) WebGL globe lib:{" "}
+        <a href="https://github.com/shuding/cobe" target="_blank">
+          GitHub
+        </a>
+      </p>
+      <canvas
+        ref={canvasRef}
+        style={{ width: 600, height: 600, maxWidth: "100%", aspectRatio: 1 }}
+      />
+    </div>
+  );
+}
+
+export default App;
